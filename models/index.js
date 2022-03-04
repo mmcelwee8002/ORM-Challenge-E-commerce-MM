1e161f9088377fc1b7246922fe45318fa4c92aa8
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-03-03 20:02:03 -0600

    updated index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id'
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'procut_id'
+});
+
+
 
 module.exports = {
   Product,
